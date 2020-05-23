270381f4b566e98cb04a35a58ec4fdc244891536
logman00b44 <logman00b44@gmail.com>
2020-05-23 17:15:40 +0400

    back created

---
+++ user_form/user_form/src/test/java/logman/yello/user_form/UserFormApplicationTests.java
@@ -0,0 +1,13 @@
+package logman.yello.user_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
